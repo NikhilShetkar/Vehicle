92a2d296192448a273fd05945c4cc94d1261bf45
sysadmin <sysadmin@bcalab1-m11>
2021-04-08 08:48:13 +0530

    first commit

---
+++ Vehicle/src/car.java
@@ -0,0 +1,15 @@
+
+public class car {
+	String brand; 
+	int speed; 
+	void Car (String brand, int gears) 
+	{ 
+	this.brand = brand; 
+	this.speed = speed; 
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
